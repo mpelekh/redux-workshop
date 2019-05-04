86a6a8ad816c6b10acde2753c9d237083797d99d
Mykola Pelekh <mpelekh@lohika.com>
2019-05-04 21:59:56 +0300

    Add initial state to store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { createStore } from 'redux'
 import rootReducer from './modules'
-
-const initialState = {}
+import initialState from './samples/initial-data'
 
 const store = createStore(
   rootReducer,
