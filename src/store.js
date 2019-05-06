97eca5b34062541268a7ac8d247b5091d9ca8e03
Mykola Pelekh <mpelekh@lohika.com>
2019-05-06 17:53:13 +0300

    Add redux-thunk

--- src/store.js
+++ src/store.js
@@ -2,11 +2,12 @@ import { createStore, applyMiddleware} from 'redux'
 import rootReducer from './modules'
 import initialState from './samples/initial-data'
 import logger from './middleware/logger'
+import thunk from 'redux-thunk';
 
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(logger)
+  applyMiddleware(thunk, logger)
 )
 
 export default store
