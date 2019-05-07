cac3ec325f754fd043a6a45eed26285bf229019f
Mykola Pelekh <mpelekh@lohika.com>
2019-05-07 22:26:53 +0300

    Clean code

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './modules'
 import initialState from './samples/initial-data'
 import logger from './middleware/logger'
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'
 
 const store = createStore(
   rootReducer,
